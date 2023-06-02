f0afd71a996ff7233d79c53f0fafd6499d1537ce
Michal Uzdowski <uzdowski.design@gmail.com>
2023-06-02 17:12:57 +0200

    sections margins

--- components/Details/Details.js
+++ components/Details/Details.js
@@ -4,7 +4,7 @@ import StatsSection from '@components/StatsSection';
 const Details = ({ country, latestStats }) => {
   return (
     <div className="absolute z-10 top-[80%] vertical_light_gradient w-full text-center min-h-[85vh] rounded-t-lg gradient_line">
-      <h2 className="text-4xl my-2">{country}</h2>
+      <h2 className="text-4xl mt-3">{country}</h2>
       {latestStats.total && (
         <StatsSection
           title={'Statystyki ogólne'}
--- components/StatsSection/StatsSection.js
+++ components/StatsSection/StatsSection.js
@@ -3,7 +3,7 @@ import TitleWrapper from '@components/TitleWrapper/TitleWrapper';
 
 const StatsSection = ({ title, stats, statsGroup }) => {
   return (
-    <div className="my-12">
+    <div className="my-10">
       <TitleWrapper title={title} />
       <StatsList stats={stats} statsGroup={statsGroup} />
     </div>
