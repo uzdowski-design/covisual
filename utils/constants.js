d41444631c041a4e6e9844bf911d98b876dc313d
Michal Uzdowski <uzdowski.design@gmail.com>
2023-06-06 16:38:50 +0200

    changed map regions colors

--- utils/constants.js
+++ utils/constants.js
@@ -58,7 +58,7 @@ export const GLOBAL_STATS_NAMES = {
 };
 
 export const MAP_LEVELS_COLORS = {
-    LOW: '#bfdbfe', // blue 200
-    MEDIUM: '#3b82f6', // blue 500
-    HIGH: '#1e40af' // blue 800
+    LOW: '#93c5fd', // blue 300
+    MEDIUM: '#2563eb', // blue 600
+    HIGH: '#1e3a8a' // blue 900
 };
